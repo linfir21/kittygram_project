04c38e75667c58202d05b293df75ece3e05ebeb6
Alex Melnikov <coyotete21@gmail.com>
2026-05-11 13:54:19 +0300

    moded

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ python3 -m venv env
 ```
 
 ```
-source env/bin/activate
+source env/Scripts/activate
 ```
 
 Установить зависимости из файла requirements.txt:
--- cats/views.py
+++ cats/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Cat
 from .serializers import CatSerializer
@@ -9,7 +10,7 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=isinstance(request.data, list))
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -17,3 +18,30 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    cat = get_object_or_404(Cat, pk=pk)
+
+    if request.method == 'GET':
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
